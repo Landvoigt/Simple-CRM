e5cbbb8b83fdde2cbd09d56fe3bf792b543943f7
Landvoigt <tim.voigt@ik.me>
2023-08-20 12:22:13 +0200

    update docs

--- src/app/dialog-edit-address/dialog-edit-address.component.ts
+++ src/app/dialog-edit-address/dialog-edit-address.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { Firestore, doc, getDoc, updateDoc } from '@angular/fire/firestore';
+import { Firestore, collectionSnapshots, doc, onSnapshot, setDoc, updateDoc } from '@angular/fire/firestore';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { User } from 'src/models/user.class';
 
 @Component({
@@ -8,11 +9,12 @@ import { User } from 'src/models/user.class';
   templateUrl: './dialog-edit-address.component.html',
   styleUrls: ['./dialog-edit-address.component.scss']
 })
-export class DialogEditAddressComponent implements OnInit{
+export class DialogEditAddressComponent implements OnInit {
   firestore: Firestore = inject(Firestore);
   user = new User();
   userID = '';
   loading: boolean = false;
+  users!: Observable<User[]>;
 
   constructor(public dialogRef: MatDialogRef<DialogEditAddressComponent>) { }
 
@@ -25,8 +27,16 @@ export class DialogEditAddressComponent implements OnInit{
     const newData = this.user.toJSON();
 
     await updateDoc(userDocRef, newData);
-    
+
+    this.user = new User(newData);
+
+    const unsubscribe = onSnapshot(userDocRef, (docSnapshot) => {
+      this.user = docSnapshot.data() as User;
+    });
+
     this.dialogRef.close();
     this.loading = false;
+
+    unsubscribe();
   }
 }
